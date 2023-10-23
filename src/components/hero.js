f45a0ea9ec1feef997816a02d9e71765ec9613b5
Indra John <indrajohn2@gmail.com>
2023-10-23 17:11:21 +1100

    fix

--- src/components/hero.js
+++ src/components/hero.js
@@ -118,7 +118,7 @@ function Hero() {
               </span>
               <div className="my-6">
                 <a
-                  href="/CV_YOSEPH_SAPUTRA.pdf"
+                  href="/YosephIndraJohnSaputraResume.pdf"
                   download
                   className="bg-[#0ef] rounded-lg text-[#1f242d] px-4 py-3 text-md font-bold"
                 >
